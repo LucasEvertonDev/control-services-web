57f3d0ec002e7652408cd3f1d200e77850f7218f
Lcs Everton <lcseverton@outlook.com>
2023-12-23 09:01:01 -0300

    incluir modulo com importações comuns do formulario

--- src/app/shared/modules/angular-material.module.ts
+++ src/app/shared/modules/angular-material.module.ts
@@ -1,27 +1,32 @@
-import { RegistrarComponent } from './components/registrar/registrar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatError, MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
-import { UsuariosRoutingModule } from './usuarios-routing.module';
-import { LoginComponent } from './components/login/login.component';
-import { AngularMaterialModule } from 'src/app/shared/modules/angular-material.module';
-
-
 @NgModule({
-  declarations: [
-    RegistrarComponent,
-    LoginComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
-    UsuariosRoutingModule,
-    AngularMaterialModule
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [
     {
@@ -32,4 +37,4 @@ import { AngularMaterialModule } from 'src/app/shared/modules/angular-material.m
   }
   ]
 })
-export class UsuariosModule { }
+export class AngularMaterialModule { }
