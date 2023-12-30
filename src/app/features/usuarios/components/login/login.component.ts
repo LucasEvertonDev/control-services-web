26eadda33b90f8c22e51b12aab09e6e9a9b22f56
Lcs Everton <lcseverton@outlook.com>
2023-12-30 12:10:47 -0300

    ajuste final do primeiro crud

--- src/app/features/usuarios/components/login/login.component.ts
+++ src/app/features/usuarios/components/login/login.component.ts
@@ -2,18 +2,19 @@ import { LoginRequest } from './../../../../core/api/services/auth-endpoint/requ
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { take } from 'rxjs';
+import { firstValueFrom, take } from 'rxjs';
 import { AuthApiService } from 'src/app/core/api/services/auth-endpoint/auth-api.service';
 import { AvisoService } from 'src/app/shared/services/snackbar.service';
 import { LoginContantsService } from '../../services/login-contants.service';
 import { FormLogin } from '../../models/form-login.model';
+import { AuthorizationService } from 'src/app/core/services/authorization.services';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   public formLogin!: FormGroup<FormLogin>;
   private loginRequest!: LoginRequest;
 
@@ -22,8 +23,15 @@ export class LoginComponent {
     private router: Router,
     private AvisoService: AvisoService,
     private authApiService: AuthApiService,
-    public LOGIN_CONSTS: LoginContantsService) {
-      this.formLogin = this.formBuilder.group<FormLogin>(new FormLogin());
+    public LOGIN_CONSTS: LoginContantsService,
+    private authorization: AuthorizationService) {
+  }
+  public async ngOnInit(): Promise<void> {
+    if (await this.estaLogado()) {
+      this.router.navigateByUrl('dashboard')
+    }
+
+    this.formLogin = this.formBuilder.group<FormLogin>(new FormLogin());
   }
 
   public onSubmit() {
@@ -41,5 +49,9 @@ export class LoginComponent {
           this.router.navigate(['/home'], { state: { prevPage: '/home' } });
         }
       });
+  }
+
+  public async estaLogado(): Promise<boolean> {
+    return await firstValueFrom(this.authorization.usuarioEstaLogado().pipe(take(1)));
   }
 }
