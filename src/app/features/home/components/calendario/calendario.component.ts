16dc5e5022646f2b78077e00db7719140167cd4a
Lucas Everton Santos de Oliveira <lucas.oliveira-ext@fiven.com>
2024-01-12 13:36:42 -0300

    atualização do calendário

--- src/app/features/home/components/calendario/calendario.component.ts
+++ src/app/features/home/components/calendario/calendario.component.ts
@@ -14,8 +14,8 @@ import { take } from 'rxjs';
 })
 export class CalendarioComponent {
   private atendimentos: EventInput[] = [];
-  calendarVisible = signal(true);
-  calendarOptions = signal<CalendarOptions>({
+  public calendarVisible = signal(true);
+  public calendarOptions = signal<CalendarOptions>({
     plugins: [
       interactionPlugin,
       dayGridPlugin,
@@ -66,17 +66,20 @@ export class CalendarioComponent {
     eventClick: this.handleEventClick.bind(this),
     eventsSet: this.handleEvents.bind(this),
     eventChange: ((arg: EventChangeArg) => {
+      //arg.oldEvent._context.calendarApi.view.type;
       console.log(arg);
-    }) 
+      // arg.event.start,
+      // arg.event.id
+    }),
     /* you can update a remote database when these fire:
     eventAdd:
     eventChange:
     eventRemove:
     */
   });
-  currentEvents = signal<EventApi[]>([]);
+  public currentEvents = signal<EventApi[]>([]);
 
-  constructor(private changeDetector: ChangeDetectorRef, 
+  public constructor(private changeDetector: ChangeDetectorRef, 
     private atendimentosApiService: AtendimentoApiService) {
       this.atendimentosApiService.getAtendimentos(1, 10000, {})
         .pipe(take(1))
@@ -99,71 +102,47 @@ export class CalendarioComponent {
         });
   }
 
-  handleCalendarToggle() {
+  public handleCalendarToggle() {
     this.calendarVisible.update((bool) => !bool);
   }
 
-  handleWeekendsToggle() {
+  public handleWeekendsToggle() {
     this.calendarOptions.update((options) => ({
       ...options,
       weekends: !options.weekends,
     }));
   }
 
-  handleDateSelect(selectInfo: DateSelectArg) {
-    const title = prompt('Please enter a new title for your event');
-    const calendarApi = selectInfo.view.calendar;
+  public remarcarAgendamento() {
 
-    calendarApi.unselect(); // clear date selection
-
-    if (title) {
-      calendarApi.addEvent({
-        id: createEventId(),
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay
-      });
-    }
   }
 
-  handleEventClick(clickInfo: EventClickArg) {
+  public handleDateSelect(selectInfo: DateSelectArg) {
+    // const title = prompt('Please enter a new title for your event');
+    // const calendarApi = selectInfo.view.calendar;
+
+    // calendarApi.unselect(); // clear date selection
+
+    // if (title) {
+    //   calendarApi.addEvent({
+    //     id: createEventId(),
+    //     title,
+    //     start: selectInfo.startStr,
+    //     end: selectInfo.endStr,
+    //     allDay: selectInfo.allDay
+    //   });
+    // }
+  }
+
+  public handleEventClick(clickInfo: EventClickArg) {
     if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
       clickInfo.event.remove();
     }
   }
 
-  handleEvents(events: EventApi[]) {
+  public handleEvents(events: EventApi[]) {
     this.currentEvents.set(events);
-    console.log(JSON.stringify(events));
+    // console.log(JSON.stringify(events));
     this.changeDetector.detectChanges(); // workaround for pressionChangedAfterItHasBeenCheckedError
   }
-}
-
-
-let eventGuid = 0;
-const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
-
-export const INITIAL_EVENTS: EventInput[] = [
-  {
-    id: createEventId(),
-    title: 'All-day event',
-    start: TODAY_STR
-  },
-  {
-    id: createEventId(),
-    title: 'Timed event',
-    start: TODAY_STR + 'T00:00:00',
-    end: TODAY_STR + 'T03:00:00'
-  },
-  {
-    id: createEventId(),
-    title: 'Timed event',
-    start: TODAY_STR + 'T12:00:00',
-    end: TODAY_STR + 'T15:00:00'
-  }
-];
-
-export function createEventId() {
-  return String(eventGuid++);
 }
