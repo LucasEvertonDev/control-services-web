8dd10b75b9eb2b85c7963af186a03163a4483440
Lucas Everton Santos de Oliveira <lucas.oliveira-ext@fiven.com>
2024-01-26 10:36:23 -0300

    Incluir htmlpdf

--- src/app/features/home/components/dashboard/dashboard.component.ts
+++ src/app/features/home/components/dashboard/dashboard.component.ts
@@ -1,16 +1,17 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { take } from 'rxjs';
 import { AtendimentoApiService } from 'src/app/core/api/services/atendimentos-endpoint/atendimentos-api.service';
 import { TotalizadoresResponse } from 'src/app/core/api/services/atendimentos-endpoint/responses/totalizadores.response';
-import html2canvas from 'html2canvas';
-import jspdf from 'jspdf';
+import * as html2pdf from 'html2pdf.js';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent  {
+export class DashboardComponent {
+  @ViewChild('pdfContent') pdfContent!: ElementRef;
   public date: Date = new Date();
   public totalizador: TotalizadoresResponse = {
     agendados: 0,
@@ -21,31 +22,26 @@ export class DashboardComponent  {
 
   public constructor(private atendimentoApiService: AtendimentoApiService) {
     this.atendimentoApiService.getTotalizadores()
-    .pipe(take(1))
-    .subscribe((response) => {
-      if(response) {
-        this.totalizador = response.content;
-      }
-    });
+      .pipe(take(1))
+      .subscribe((response) => {
+        if (response) {
+          this.totalizador = response.content;
+        }
+      });
   }
 
+  sendToPdf() {
+    const content = this.pdfContent.nativeElement;
 
+    const options = {
+      useCORS: true,
+      margin: 0,
+      filename: 'document.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 3 },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+    };
 
-sendToPdf(){
-  let data = document.getElementById("test"); 
-
-  if(!data) {
-    return;
+    html2pdf().from(content).set(options).save();
   }
-    // let data = document.getElementById("maindiv");
-    console.log(data);  
-    html2canvas(data).then(canvas => {
-      const contentDataURL = canvas.toDataURL('image/jpeg', 1.0)
-      console.log(contentDataURL);  
-      let pdf = new jspdf('l', 'cm', 'a4'); //Generates PDF in landscape mode
-      // let pdf = new jspdf('p', 'cm', 'a4'); //Generates PDF in portrait mode
-      pdf.addImage(contentDataURL, 'PNG', 0, 0, 29.7, 21.0);  
-      pdf.save('Filename.pdf');   
-    }); 
-}
 }
