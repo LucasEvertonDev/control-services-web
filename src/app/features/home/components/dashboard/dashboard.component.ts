246e96c87ef45c7de4682efbe01bb42354349378
Lucas Everton Santos de Oliveira <lucas.oliveira-ext@fiven.com>
2024-01-26 13:34:20 -0300

    correção 2

--- src/app/features/home/components/dashboard/dashboard.component.ts
+++ src/app/features/home/components/dashboard/dashboard.component.ts
@@ -2,7 +2,8 @@ import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular
 import { take } from 'rxjs';
 import { AtendimentoApiService } from 'src/app/core/api/services/atendimentos-endpoint/atendimentos-api.service';
 import { TotalizadoresResponse } from 'src/app/core/api/services/atendimentos-endpoint/responses/totalizadores.response';
-import * as html2pdf from 'html2pdf.js';
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 
 @Component({
@@ -11,8 +12,8 @@ import * as html2pdf from 'html2pdf.js';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-  @ViewChild('pdfContent') pdfContent!: ElementRef;
-  public date: Date = new Date();
+  @ViewChild('conteudoParaPDF') conteudoParaPDF!: ElementRef; 
+   public date: Date = new Date();
   public totalizador: TotalizadoresResponse = {
     agendados: 0,
     concluidos: 0,
@@ -30,18 +31,41 @@ export class DashboardComponent {
       });
   }
 
+  // sendToPdf2() {
+  //   const content = this.pdfContent.nativeElement;
+
+  //   const options = {
+  //     useCORS: true,
+  //     margin: 0,
+  //     filename: 'document.pdf',
+  //     image: { type: 'jpeg', quality: 0.98 },
+  //     html2canvas: { scale: 2 },
+  //     jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+  //   };
+
+  //   html2pdf().from(content).set(options).save();
+  // }
+
   sendToPdf() {
-    const content = this.pdfContent.nativeElement;
+    const content = this.conteudoParaPDF.nativeElement;
 
-    const options = {
-      useCORS: true,
-      margin: 0,
-      filename: 'document.pdf',
-      image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 3 },
-      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
-    };
+    if(!content) return;
 
-    html2pdf().from(content).set(options).save();
+    html2canvas(content, { scale:4, backgroundColor: '#eee' }).then((canvas) => {
+      // Configuração do PDF
+      const pdf = new jsPDF.jsPDF('p', 'mm', 'a4');
+      const imgData = canvas.toDataURL('image/png');
+      const imgWidth = 210;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+      // Adiciona a imagem convertida ao PDF
+      pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+
+      // Adicione mais conteúdo ou páginas conforme necessário
+      // Exemplo: pdf.addPage(); pdf.text('Outro conteúdo', 10, 10);
+
+      // Salva ou exibe o PDF
+      pdf.save('documento.pdf');
+    });
   }
 }
